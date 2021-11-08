13bc7c1a32077e2e2e2e157c6b6de959d3620d74
MrTehnical <69931156+MrTehnical@users.noreply.github.com>
2021-11-08 12:08:20 -0800

    v11

--- conv_old.c
+++ conv_old.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
 const char *ascii = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
 
@@ -18,17 +17,6 @@ int power2Check (int base)
 }
 
 int length(char *ptr){
-  int i = 0;
-  int n = 0;
-  while(*(ptr + i) !='\0')
-  {
-    ++n;
-    ++i;
-  }
-  return n;
-}
-
-int length2(int *ptr){
   int i = 0;
   int n = 0;
   while(*(ptr + i) !='\0')
@@ -56,8 +44,6 @@ void other_base(int decimal, int base)
   int temp = decimal;
 
     int arr[36];
-		char buffer[36];
-		int len;
     int r;
 
     int i = 0;
@@ -77,26 +63,19 @@ void other_base(int decimal, int base)
         }
     }
     i--;
-		len = length(carr);
-		i = 0;
+    while (i+1) {
+        printf("%c", carr[i]);
+        i--;
+    }
+    printf("\n");
 
-		while(len > 0)
-		{
-			buffer[len - 1] = carr[i];
-			len--;
-			i++;
-		}
-
-		printf("%s\n", buffer);
 }
 
 void power2_base(int decimal, int base)
 {
-	int temp = decimal;
+  int temp = decimal;
 
     int arr[36];
-		char buffer[36];
-		int len;
     int r;
 
     int i = 0;
@@ -110,82 +89,42 @@ void power2_base(int decimal, int base)
     char carr[100];
     for (int n = 0; n < i; n++) {
         if (arr[n] > 9) {
-            carr[n] = arr[n] + 'A'- 10;
+            carr[n] = arr[n] + 'A' - 10;
         } else {
             carr[n] = arr[n] + '0';
         }
     }
-
-		len = length(carr);
-		i = 0;
-
-		while(len > 0)
-		{
-			buffer[len - 1] = carr[i];
-			len--;
-			i++;
-		}
-
-    buffer[i] = '\0';
-
-		printf("%s\n", buffer);
-}
-
-void base_256(const long long decimal, const int base)
-{
-  //int temp = decimal;
-	char buffer[36];
-	char buffer2[36];
-	double test;
-
-      long long m = decimal;
-      long long b = base;
-      int i = 0;
-
-      long long temp = m;
-
-      while (temp > 0) {
-          i++;
-          temp /= b;
-      }
-
-      i++;
-
-      long long parquos[i];
-
-      temp = m;
-      for (int j = 0; j < i; j++) {
-          parquos[j] = temp;
-          temp /= b;
-
-      }
-
-      long long arr[i];
-
-      for (int j = 0; j < i; j++) {
-          arr[j] = parquos[j] % b;
-      }
-
     i--;
-    i--;
-		int c = 0;
-		int d = 0;
-    while (i + 1) {
-				sprintf(buffer2, "%lld", arr[i]);
-				d = 0;
-				while(buffer2[d] != '\0'){
-        buffer[c] = buffer2[d];
-				c++;
-				d++;
-				}
-        if (i){
-        buffer[c] = '.';
-				c++;
-				}
+    while (i+1) {
+        printf("%c", carr[i]);
         i--;
     }
-    buffer[c] = '\0';
-    printf("%s\n", buffer);
+    printf("\n");
+}
+
+void base_256(int decimal, int base)
+{
+  int temp = decimal;
+
+    int arr[36];
+    int r;
+
+    int i = 0;
+    while (temp > 0) {
+        r = temp % base;
+        arr[i] = r;
+        temp /= base;
+        i++;
+    }
+
+    i--;
+    while (i+1) {
+        printf("%d", arr[i]);
+        if (i)
+        printf(".");
+        i--;
+    }
+    printf("\n");
 }
 
 
@@ -198,15 +137,20 @@ int main(int argc, char **argv){
   char *decimal = argv[1];
   int pcheck = 0;
 
+
+
+
 	int base_int = 0;
   for (int i = 0; i < length(base); i++)
   {
     base_int += power(10, length(base) -1 - i) * (base[i] - '0');
   }
 
-  long long decimal_int = 0;
-
-  decimal_int = atoll(decimal);
+  int decimal_int = 0;
+  for (int i = 0; i < length(decimal); i++)
+  {
+    decimal_int += power(10, length(decimal) -1 - i) * (decimal[i] - '0');
+  }
 
   pcheck = power2Check(base_int);
 
